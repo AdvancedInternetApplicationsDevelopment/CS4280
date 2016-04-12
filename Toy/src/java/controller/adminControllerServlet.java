744fddab28f4c685f254caacde0c463c9820f5f8
suhag <suhag@DESKTOP-FVNJNFU>
2016-04-13 03:07:17 +0800

    adminEdit done

--- Toy/src/java/controller/adminControllerServlet.java
+++ Toy/src/java/controller/adminControllerServlet.java
@@ -7,7 +7,8 @@ package controller;
 
 import dbaccessor.*;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -25,14 +26,15 @@ import model.Product;
  * @author suhag
  */
 public class adminControllerServlet extends HttpServlet {
+
     private String userPath;
     private int noOfUsers;
     private int pendingApprovals;
     private int productsSold;
     private int totalNoOfProducts;
     private Product latestApprovalItem;
-    private List<OrderHistory> transactions; 
-    
+    private List<OrderHistory> transactions;
+
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -47,33 +49,27 @@ public class adminControllerServlet extends HttpServlet {
             throws ServletException, IOException {
         HttpSession session = request.getSession(true);
         userPath = request.getServletPath();
-        
+
         if (userPath.equals("/adminDashboard")) {
-            
-            CustomerDAO customerDAO = new CustomerDAO();
-            ProductDAO productDAO = new ProductDAO();
-            OrderedProductDAO orderedProductDAO = new OrderedProductDAO();
-            OrderHistoryDAO orderHistoryDAO = new OrderHistoryDAO();
+
+            CustomerDAO customerDAO = new CustomerDAOImpl();
+            ProductDAO productDAO = new ProductDAOImpl();
+            OrderedProductDAO orderedProductDAO = new OrderedProductDAOImpl();
+            OrderHistoryDAO orderHistoryDAO = new OrderHistoryDAOImpl();
             noOfUsers = (customerDAO).getNoOfUsers();
             pendingApprovals = (productDAO).getAllPending().size();
             productsSold = (orderedProductDAO).getTotalQuantityOfProductSold();
             totalNoOfProducts = (productDAO).getNoOfProducts();
             latestApprovalItem = (productDAO).getLatestPending();
             transactions = (orderHistoryDAO).getLatest();
-                        
+
             request.setAttribute("noOfUsers", noOfUsers);
             request.setAttribute("pendingApprovals", pendingApprovals);
             request.setAttribute("productsSold", productsSold);
             request.setAttribute("totalNoOfProducts", totalNoOfProducts);
             request.setAttribute("latestApprovalItem", latestApprovalItem);
             request.setAttribute("transactions", transactions);
-            customerDAO.closeDB();
-            productDAO.closeDB();
-            orderHistoryDAO.closeDB();
-            orderedProductDAO.closeDB();
-        }
-        
-        else if (userPath.equals("/adminCustomer")) {
+        } else if (userPath.equals("/adminCustomer")) {
             List<Customer> customerSearchList = null;
             request.setAttribute("FName", "");
             request.setAttribute("LName", "");
@@ -85,64 +81,70 @@ public class adminControllerServlet extends HttpServlet {
             request.setAttribute("country", "");
             request.setAttribute("region", "");
             request.setAttribute("customerSearchList", customerSearchList);
-        }
-        
-        else if(userPath.equals("/adminTransactions"))
-        {
+        } else if (userPath.equals("/adminTransactions")) {
             List<OrderHistory> orderHistorys = null;
             request.setAttribute("transactions", orderHistorys);
             request.setAttribute("orderNo", "");
             request.setAttribute("userId", "");
-        }
-        
-        else if(userPath.equals("/adminProducts"))
-        {
+        } else if (userPath.equals("/adminProducts")) {
             List<Product> products = null;
             request.setAttribute("pName", "");
             request.setAttribute("mNo", "");
             request.setAttribute("categoryId", -1);
             request.setAttribute("products", products);
-        }
-        else if(userPath.equals("/adminRecycledProducts"))
-        {
+        } else if (userPath.equals("/adminRecycledProducts")) {
             List<Product> products = null;
             request.setAttribute("pName", "");
             request.setAttribute("mNo", "");
             request.setAttribute("categoryId", -1);
             request.setAttribute("owner", "");
             request.setAttribute("products", products);
-        }
-        
-        else if(userPath.equals("/adminApprovals"))
-        {
-            ProductDAO productDAO = new ProductDAO();
+        } else if (userPath.equals("/adminApprovals")) {
+            ProductDAO productDAO = new ProductDAOImpl();
             List<Product> approveProducts = productDAO.getAllPending();
             request.setAttribute("approveProducts", approveProducts);
-            
-        }
-        
-        else if(userPath.equals("/adminViewRecycled"))
-        {
+
+        } else if (userPath.equals("/adminViewRecycled")) {
             String productId = request.getParameter("productId");
-            ProductDAO productDAO = new ProductDAO();
+            ProductDAO productDAO = new ProductDAOImpl();
             Product product = productDAO.getProductFromID(productId);
             request.setAttribute("product", product);
+        } else if (userPath.equals("/adminEditProducts")) {
+            String productId = request.getParameter("productId");
+            ProductDAO productDAO = new ProductDAOImpl();
+            Product product = productDAO.getProductFromID(productId);
+            if (product == null) {
+                userPath = "/admin404";
+            } else {
+                request.setAttribute("pName", product.getName());
+                request.setAttribute("mNo", product.getModelNum());
+                request.setAttribute("brand", product.getBrand());
+                request.setAttribute("description", product.getDescription());
+                request.setAttribute("quantity", product.getQuantity());
+                request.setAttribute("price", product.getPrice());
+                request.setAttribute("addInfo", product.getAddInfo());
+                request.setAttribute("categoryId", product.getCategoryId().getId());
+                request.setAttribute("productId", product.getId());
+                request.setAttribute("success", false);
+                request.setAttribute("error", false);
+                request.setAttribute("errorMessage", null);
+            }
+
         }
-        
+
         String url = "/WEB-INF/view" + userPath + ".jsp";
         try {
             request.getRequestDispatcher(url).forward(request, response);
         } catch (ServletException ex) {
 //            String urlNotFound = "/WEB-INF/view/admin404.jsp";
 //            request.getRequestDispatcher(urlNotFound).forward(request, response);
-              ex.printStackTrace();
+            ex.printStackTrace();
         } catch (IOException ex) {
 //            String urlNotFound = "/WEB-INF/view/admin404.jsp";
 //            request.getRequestDispatcher(urlNotFound).forward(request, response);
-                ex.printStackTrace();
+            ex.printStackTrace();
         }
-        
-        
+
     }
 
     /**
@@ -158,7 +160,7 @@ public class adminControllerServlet extends HttpServlet {
             throws ServletException, IOException {
         userPath = request.getServletPath();
         if (userPath.equals("/adminCustomer")) {
-            
+
             String FName = (String) request.getParameter("FName");
             String LName = (String) request.getParameter("LName");
             String emailID = (String) request.getParameter("emailID");
@@ -168,77 +170,105 @@ public class adminControllerServlet extends HttpServlet {
             String postalCode = (String) request.getParameter("postalCode");
             String country = (String) request.getParameter("country");
             String region = (String) request.getParameter("region");
-            System.out.print(emailID+FName+ LName+ phone+ fax+ city+ postalCode+ country+ region);
+            System.out.print(emailID + FName + LName + phone + fax + city + postalCode + country + region);
             List<Customer> customerSearchList = null;
-            if(!( FName.equalsIgnoreCase("") && LName.equalsIgnoreCase("") && emailID.equalsIgnoreCase("") && phone.equalsIgnoreCase("") && fax.equalsIgnoreCase("") && city.equalsIgnoreCase("") && country.equalsIgnoreCase("") && region.equalsIgnoreCase("") && postalCode.equalsIgnoreCase(""))){
-                CustomerDAO customerDAO = new CustomerDAO();
+            if (!(FName.equalsIgnoreCase("") && LName.equalsIgnoreCase("") && emailID.equalsIgnoreCase("") && phone.equalsIgnoreCase("") && fax.equalsIgnoreCase("") && city.equalsIgnoreCase("") && country.equalsIgnoreCase("") && region.equalsIgnoreCase("") && postalCode.equalsIgnoreCase(""))) {
+                CustomerDAO customerDAO = new CustomerDAOImpl();
                 customerSearchList = customerDAO.getByFilter(emailID, FName, LName, phone, fax, city, postalCode, country, region);
-                customerDAO.closeDB();
             }
             request.setAttribute("FName", FName);
             request.setAttribute("LName", LName);
-            request.setAttribute("emailID",emailID);
+            request.setAttribute("emailID", emailID);
             request.setAttribute("phone", phone);
-            request.setAttribute("fax",fax);
-            request.setAttribute("city",city);
+            request.setAttribute("fax", fax);
+            request.setAttribute("city", city);
             request.setAttribute("postalCode", postalCode);
-            request.setAttribute("country",country);
+            request.setAttribute("country", country);
             request.setAttribute("region", region);
             request.setAttribute("customerSearchList", customerSearchList);
-        }
-        
-        else if(userPath.equals("/approveProduct"))
-        {
-            ProductDAO productDAO = new ProductDAO();
+        } else if (userPath.equals("/approveProduct")) {
+            ProductDAO productDAO = new ProductDAOImpl();
             productDAO.approveRecycled(request.getParameter("productId"));
-            productDAO.closeDB();
             userPath = "/adminDashboard";
-        }
-        
-        else if(userPath.equals("/adminTransactions"))
-        {
+        } else if (userPath.equals("/adminTransactions")) {
             String customerId = request.getParameter("userId");
             String orderNo = request.getParameter("orderNo");
-            OrderHistoryDAO orderHistoryDAO = new OrderHistoryDAO();
+            OrderHistoryDAO orderHistoryDAO = new OrderHistoryDAOImpl();
             List<OrderHistory> orderHistory = orderHistoryDAO.getByFilter(customerId, orderNo);
-            orderHistoryDAO.closeDB();
             request.setAttribute("transactions", orderHistory);
             request.setAttribute("orderNo", orderNo);
             request.setAttribute("userId", customerId);
-        }
-        
-        else if(userPath.equals("/adminProducts"))
-        {
+        } else if (userPath.equals("/adminProducts")) {
             List<Product> products = null;
             String pName = request.getParameter("pName");
             String mNo = request.getParameter("mNo");
             int categoryId = Integer.parseInt(request.getParameter("category"));
-            ProductDAO productDAO = new ProductDAO();
+            ProductDAO productDAO = new ProductDAOImpl();
             products = productDAO.getByFilter(pName, mNo, categoryId);
             request.setAttribute("pName", pName);
             request.setAttribute("mNo", mNo);
             request.setAttribute("categoryId", categoryId);
             request.setAttribute("products", products);
-            
-        }
-        else if(userPath.equals("/adminRecycledProducts"))
-        {
+
+        } else if (userPath.equals("/adminRecycledProducts")) {
             List<Product> products = null;
             String pName = request.getParameter("pName");
             String mNo = request.getParameter("mNo");
             String owner = request.getParameter("owner");
             int categoryId = Integer.parseInt(request.getParameter("category"));
-            ProductDAO productDAO = new ProductDAO();
+            ProductDAO productDAO = new ProductDAOImpl();
             products = productDAO.getRecycledByFilter(pName, owner, categoryId);
             request.setAttribute("pName", pName);
             request.setAttribute("mNo", mNo);
             request.setAttribute("owner", owner);
             request.setAttribute("categoryId", categoryId);
             request.setAttribute("products", products);
-            
+
+        } else if (userPath.equals("/adminEditProducts")) {
+
+            boolean success = true;
+            ProductDAO productDAO = new ProductDAOImpl();
+            Product product;
+            String productId = request.getParameter("productId");
+            String pName = request.getParameter("pName");
+            String mNo = request.getParameter("mNo");
+            String brand = request.getParameter("brand");
+            String description = request.getParameter("description");
+            String quantity = request.getParameter("quantity");
+            String price = request.getParameter("price");
+            String addInfo = request.getParameter("addInfo");
+            int categoryId = Integer.parseInt(request.getParameter("category"));
+
+            try {
+                double priceDouble = Double.parseDouble(price);
+                int quantityInt = Integer.parseInt(quantity);
+                Category category = (new CategoryDAOImpl()).getCategoryFromID(categoryId);
+                Timestamp lastUpdate = new Timestamp((new Date()).getTime());
+                product = new Product(productId, pName, mNo, category, quantityInt, true, priceDouble, brand, description, addInfo, lastUpdate, true, true, "shop");
+                productDAO.updateProduct(product);
+            } catch (Exception e) {
+                request.setAttribute("error", true);
+                request.setAttribute("errorMessage", e.getMessage());
+                success = false;
+            }
+
+            request.setAttribute("pName", pName);
+            request.setAttribute("mNo", mNo);
+            request.setAttribute("brand", brand);
+            request.setAttribute("description", description);
+            request.setAttribute("quantity", quantity);
+            request.setAttribute("price", price);
+            request.setAttribute("addInfo", addInfo);
+            request.setAttribute("categoryId", categoryId);
+            request.setAttribute("productId", productId);
+            request.setAttribute("success", success);
+            if (success == true) {
+                request.setAttribute("error", false);
+                request.setAttribute("errorMessage", null);
+            }
+
         }
-        
-        
+
         String url = "/WEB-INF/view" + userPath + ".jsp";
         try {
             request.getRequestDispatcher(url).forward(request, response);
@@ -263,11 +293,9 @@ public class adminControllerServlet extends HttpServlet {
     public void init() throws ServletException {
         super.init(); //To change body of generated methods, choose Tools | Templates.
         ServletContext application = getServletContext();
-        CategoryDAO categoryDAO = new CategoryDAO();
+        CategoryDAO categoryDAO = new CategoryDAOImpl();
         List<Category> categories = categoryDAO.getAll();
-        categoryDAO.closeDB();
         application.setAttribute("categories", categories);
     }
-    
 
 }
