9c2cd52f42c84cb6a49f100c89d3763d795d229c
suhag <suhag@DESKTOP-FVNJNFU>
2016-04-19 22:13:06 +0800

    compare and wish list done

--- Toy/src/java/controller/clientSideServlet.java
+++ Toy/src/java/controller/clientSideServlet.java
@@ -5,13 +5,10 @@
  */
 package controller;
 
-import com.sun.javafx.scene.control.skin.VirtualFlow;
 import dbaccessor.*;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
